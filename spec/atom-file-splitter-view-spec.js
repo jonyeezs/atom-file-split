dc9077d663dc5c66df1d071fabc3bdce03032606
Jonathan Yee <jonyeezs@gmail.com>
2018-09-21 14:37:25 +1000

    :tada: (scaffold) Scaffold package and skeleton of app

---
+++ spec/atom-file-splitter-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomFileSplitterView from '../lib/atom-file-splitter-view';
+
+describe('AtomFileSplitterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
